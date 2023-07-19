e1055c0f91089674b76311fdf3f842e2a9d9a805
stiven jimenez <sajqprogramming@gmail.com>
2023-07-18 22:20:14 -0500

    💻 add the colors of the light mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      "White-Lavender": "#F6F8FF",
+      "Pure-White": "#FEFEFE",
+      "Bright-Blue": "#0079FF",
+      "Sky-Blue": "#60ABFF",
+      "Steel-Blue": "#4B6A9B",
+      "Light-Red": "#F74646",
+      "Dark-Blue": "#2B3442",
+      "Deep-Black": "#222731",
+    },
   },
   plugins: [],
 };
