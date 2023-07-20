4e8f7f58fae0d5afea45e41f9991defaffb2af1a
stiven jimenez <sajqprogramming@gmail.com>
2023-07-20 15:11:20 -0500

    🔧 add custom box shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        custom: " 0px 16px 30px -10px rgba(70, 96, 187, 0.20)",
+      },
+    },
 
     fontFamily: {
       "space-mono": ["Space Mono", "monospace"],
